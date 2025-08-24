4d37d0826681e58ff7d67356060e3acf72e2ea88
ING. Miguel Leguizamón <miguel.leguizamon02@gmail.com>
2025-08-24 17:22:50 -0500

    Se agrego el componente y contenedor de AboutUs

---
+++ Inventory/SGA-inventory/src/modules/LandingPage/containers/AboutUs.jsx
@@ -0,0 +1,13 @@
+import { Titles } from "../components/Titles";
+import './AboutUs.css';
+
+
+export function AboutUs(){
+    return(
+        <div className="AboutUs">
+            <Titles text={'About Us'}/>
+        </div>
+
+    )
+}
+
