30e979bda5407f209a3f49549497481d2df050ad
ING. Miguel Leguizamón <miguel.leguizamon02@gmail.com>
2025-08-26 22:01:03 -0500

    Se agregaron las cards y las imagens faltantes y se corrigieron los errores del LandingPage

--- Inventory/SGA-inventory/src/modules/LandingPage/components/SectionServices.jsx
+++ Inventory/SGA-inventory/src/modules/LandingPage/components/SectionServices.jsx
@@ -7,13 +7,15 @@ import './SectionServices.css';
 export function SectionServices() {
   return (
     <section className="SectionServices">
-        <div className="sectionServicesText">
-            <>
-                <Titles text={'Qué ofrecemos?'}/>
-                <Paragraph text={'En SGA - Inventarios buscamos ofrecer un inventario con todas sus funcionalidades pero adaptado a una nueva era tecnológica y al alcance de todos.'}/>
-            </>
-        </div>
-
+      <div className="sectionServicesText">
+          <>
+              <Titles text={'Qué ofrecemos?'}/>
+              <Paragraph text={'En SGA - Inventarios buscamos ofrecer un inventario con todas sus funcionalidades pero adaptado a una nueva era tecnológica y al alcance de todos.'}/>
+          </>
+      </div>
+      <div className="sectionServicesImage">
+        <MediaHandler name={'oso4'}/>
+      </div>
       <section className="sectionServicesIcons">
         <div className="Icon">
           <span className="icon" aria-hidden="true"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-archive"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" /><path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" /><path d="M10 12l4 0" /></svg></span>
