5ca522521f5789d1d71742fa13f13823a876c3d1
ING. Miguel Leguizamón <miguel.leguizamon02@gmail.com>
2025-08-29 21:56:58 -0500

    Se realizo la estructura del landing para la seccion AboutUs

--- Inventory/SGA-inventory/src/modules/LandingPage/containers/AboutUs.jsx
+++ Inventory/SGA-inventory/src/modules/LandingPage/containers/AboutUs.jsx
@@ -1,3 +1,4 @@
+import { AboutUsGalery } from "../components/AboutUsGalery";
 import { Titles } from "../components/Titles";
 import './AboutUs.css';
 
@@ -5,7 +6,12 @@ import './AboutUs.css';
 export function AboutUs(){
     return(
         <div className="AboutUs">
-            <Titles text={'About Us'}/>
+            <div className="AboutUsTitle">
+                <h1>SGA - <span className="highlight">INVENTARIOS</span></h1>
+            </div>
+            <div className="AboutUsGalery">
+                <AboutUsGalery/>
+            </div>
         </div>
 
     )
