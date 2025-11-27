6687a6bdc53c9f63c382b176d563c3575df1132d
José Miguel Murillo Romero <murillojose.nvc@gmail.com>
2025-11-26 21:07:05 -0500

    New update for My Bussines SGA/management

--- SGA-management/SGA-managment/src/modules/userApp/containers/MyBussines.jsx
+++ SGA-management/SGA-managment/src/modules/userApp/containers/MyBussines.jsx
@@ -3,6 +3,7 @@ import { BoldTitle } from '../components/BoldTitle';
 import { DescriptionSpan } from '../components/DescriptionSpan';
 import { CardMyBussines } from '../components/CardMyBussines';
 import {useAppInfo} from '../../../context/context'
+import { useRef, useState } from 'react';
 
 export function MyBussines(){
     const {appInfo} = useAppInfo();
@@ -30,12 +31,10 @@ export function MyBussines(){
     const VISIBLE = 4;
     // datos reales (aquí los genero con el mismo info repetido para tu ejemplo)
     const cardsData = [
-        { name: "Unidades de Negocio", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles." },
-        { name: "Centros de costo", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles." },
-        { name: "Clientes y Proveedores", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles." },
-        { name: "Personal", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles." },
-        { name: "Card 5", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles." },
-        { name: "Card 6", text: "Info 6" },
+        { name: "Unidades de Negocio", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles.", image:'https://res.cloudinary.com/djjxugmni/image/upload/v1763930815/3d-business-wallet-finance-illustration-free-png_vr9tvx.png'},
+        { name: "Centros de costo", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles.",image:'https://res.cloudinary.com/djjxugmni/image/upload/v1763930815/3d-business-wallet-finance-illustration-free-png_vr9tvx.png' },
+        { name: "Clientes y Proveedores", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles.", image:'https://res.cloudinary.com/djjxugmni/image/upload/v1763930685/ChatGPT_Image_23_nov_2025_15_41_45_xvrjtd.png'},
+        { name: "Personal", text: "Crea y administra todas las unidades de negocio, franquicias y tiendas que tengas disponibles.", image:'https://res.cloudinary.com/djjxugmni/image/upload/v1764082062/ChatGPT_Image_25_nov_2025_09_47_22_lmd5sy.png' },
     ];
 
 
@@ -129,12 +128,8 @@ export function MyBussines(){
                 <button className="carouselArrow right" onClick={handleNext} aria-label="Siguiente">
                     ›
                 </button>
-            <div className='carouselMyBussines'>
-                {info.map((element,index)=>(
-                    <CardMyBussines info={element} key={index} />
-                ))}
-            </div>
-            
+
         </div>
+    </div>
     )
 }
