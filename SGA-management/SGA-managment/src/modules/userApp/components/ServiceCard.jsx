3fa01cf05b1cec91205feafdc68fceb32b3e276b
José Miguel Murillo Romero <murillojose.nvc@gmail.com>
2025-12-06 15:04:52 -0500

    Services imgs update

--- SGA-management/SGA-managment/src/modules/userApp/components/ServiceCard.jsx
+++ SGA-management/SGA-managment/src/modules/userApp/components/ServiceCard.jsx
@@ -3,6 +3,7 @@ import './ServiceCard.css'
 export function ServiceCard({info,onClick}){
     return(
         <div onClick={onClick} className={`ServiceCard ${info.className}`}>
+            <img src={info.img} alt="" />
             <strong>{info.title}</strong>
         </div>
     )
