909b75400cfc72e1fa97408e887eb85f6035fa58
José Miguel Murillo Romero <murillojose.nvc@gmail.com>
2025-12-13 11:25:12 -0500

    Restore server version

--- api/server.js
+++ api/server.js
@@ -1,43 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes/index.routes.js';
+import routes from './routes/index.routes.js'
 
 const app = express();
-const asignedPort = process.env.PORT || 3000;
+const asignedPort  = process.env.PORT || 3000;
 
-const allowedOrigins = [
-    "https://sga-managemet.onrender.com",
-    "http://localhost:5173",
-];
-
-// Body parser
-app.use(express.json());
-
-// CORS correcto
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-            return callback(new Error("Not allowed by CORS"));
-        },
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
-    })
-);
-
-// ✅ PRE-FLIGHT FIX (EXPRESS 5)
-app.options(/.*/, cors());
-
-// Rutas
+app.use(cors());
 app.use(routes);
 
-app.listen(asignedPort, () => {
-    console.log(
-        'Servidor SGA_inventory v1.2 Escuchando en el puerto',
-        asignedPort
-    );
+app.listen(asignedPort , () => {
+    console.log('Servidor SGA_inventory v1.2 Escuchando en el puerto', asignedPort);
 });
+
