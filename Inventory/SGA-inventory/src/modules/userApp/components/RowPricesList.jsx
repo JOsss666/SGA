c69b2c0f3fef896c7c375bca0f8a9fe7aaa3cc7d
José Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-01-17 10:03:29 -0500

    New Inventory update for pricesList

--- Inventory/SGA-inventory/src/modules/userApp/components/RowPricesList.jsx
+++ Inventory/SGA-inventory/src/modules/userApp/components/RowPricesList.jsx
@@ -1,27 +1,39 @@
 
-import './RowPricesList.css'
-import { TagIndicator } from './TagIndicator'
 import { useAlert } from '../../../context/context'
-import { PreviewProduct } from '../containers/alerts/PreviewProduct';
-import { BoldButton } from './BoldButton';
 import { postInfo } from '../../../utils/functions';
 import { useState, useEffect } from 'react';
-import { EditCostValueList } from '../containers/forms/EditCostValueList';
-import { moneyFormat } from '../../../utils/functions';
+import { FormInput } from './FormInput';
+import { CheckSquare } from './CheckSquare';
+import './RowPricesList.css'
 
-export function RowPricesList({info,list_id}){
+export function RowPricesList({disabled,columns,info}){
 
     const {popInAlert,setOpenAlert} = useAlert();
     const [data,setNewInfo] = useState(info);
 
-    const isInList = info.list_id == list_id.list_id;
+    const formatCurrency = (value) =>
+        new Intl.NumberFormat("es-CO").format(value);
+
+    const isInList = true;
+
+    const dictionaryElementsColum = {
+        "SKU":<span className="rowSpan Redirect idHolder">{info.code}</span>,
+        "Producto":<span className="rowSpan Redirect">{info.product_name}</span>,
+        "Categoría":<span className="rowSpan Redirect">{info.category_name}</span>,
+        "Unidades":<span className='rowSpan'>{info.units}</span>,
+        "Costo":<span className='rowSpan'>$ {formatCurrency(info.unit_cost)}</span>,
+        "Gravado":<span className='rowSpan'>{info.taxed}</span>,
+        "MOQ":<input min={1} className='valueUpdateIn' type='number' disabled={disabled} placeholder='1'/>,
+        "Precio Venta":<input className='valueUpdateIn' type='number' disabled={disabled} placeholder='$ 0'/>,
+        "Margen":<span className='rowSpan'>0%</span>
+    }
 
     const ReloadInfo = async(info)=>{
         let res = await postInfo('/getProducts',info);
         if(res[0]){
             console.log('Actualizando información')
             setNewInfo(res[1][0]);
-        }
+        } 
         console.log(res)
     }
 
@@ -31,19 +43,12 @@ export function RowPricesList({info,list_id}){
 
     return(
         <div className={`RowPricesList ${isInList? "ActualListProduct":""}`}>
-            <span>#{data.product_code}</span>
-            <span onClick={()=>{
-                popInAlert(<PreviewProduct info={data}/>)
-                setOpenAlert(true)
-            }} className='multipleSelect'>{data.product_name}</span>
-            <span className='multipleSelect'>Nombre del Proveedor</span>
-            <span><TagIndicator/></span>
-            <span className='editableSpan'>$ {moneyFormat(data.unit_cost!= undefined? data.unit_cost:0)} </span>
-            <span className='editableSpan'>$ {moneyFormat(data.unit_value != undefined? data.unit_value:0)} <BoldButton onClick={()=>{popInAlert(<EditCostValueList reloadFun={ReloadInfo} info={data} listInfo={list_id} />);setOpenAlert(true)}} title={`Editar precio de ${data.product_name}`} children={<i className="fa-solid fa-pencil"/>}/></span>
-            <span>{data.storeStock == null? 0:data.storeStock}</span>
-            <span>{data.units}</span>
-            <span>{data.unit_value != undefined && data.unit_cost != undefined? (((data.unit_value * 100)/ data.unit_cost)-100).toFixed(2):'-- '}%</span>
-            <span>{data.min_stock == null? 0:data.min_stock}</span>
+            <CheckSquare/>
+            {columns.map((element,index)=>(
+                <div key={index} className="ElementRow">
+                    {dictionaryElementsColum[element]}
+                </div>
+            ))}
         </div>
     )
 }
