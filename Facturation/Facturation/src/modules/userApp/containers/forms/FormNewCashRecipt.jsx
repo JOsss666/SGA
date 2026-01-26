c6a5245f3ab64f39214a7dd919125f93d2c07299
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-01-26 13:05:00 -0500

    Fixed cashRecipt error

--- Facturation/Facturation/src/modules/userApp/containers/forms/FormNewCashRecipt.jsx
+++ Facturation/Facturation/src/modules/userApp/containers/forms/FormNewCashRecipt.jsx
@@ -170,6 +170,7 @@ export function FormNewCashRecipt({InfoParams,reloadFun,process_instance_id}){
     const handleSelectInstance = (element)=>{
         setInstance_id(element.id)
         setStep_id(element.step_id)
+        setThirdParty_id(element.thirdParty_id)
         setInfo(prevInfo => ({
             ...prevInfo,            // Mantenemos todas las propiedades actuales (nombre, fecha, etc.)
             thirdParty_id: element.thirdParty_id // Sobrescribimos solo el ID del tercero
