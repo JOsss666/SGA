699162591482992215cd242716c8d136cd7d1d9d
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-02-17 09:13:28 -0500

    Fixed vite build in web

--- Facturation/Facturation/vite.config.js
+++ Facturation/Facturation/vite.config.js
@@ -9,13 +9,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        facturacion: resolve(__dirname, 'Facturation/Facturation/index.html'),
-        inventario: resolve(__dirname, 'Inventory/SGA-inventory/index.html'),
+        main: resolve(__dirname, 'index.html')
+        //facturacion: resolve(__dirname, 'Facturation/Facturation/index.html'),
+        //inventario: resolve(__dirname, 'Inventory/SGA-inventory/index.html'),
         //contabilidad: resolve(__dirname, 'Inventory/index.html'),
-        procesos: resolve(__dirname, 'Process/SGA-Process/index.html'),
-        tesoreria: resolve(__dirname, 'Treasury/SGA - Treasuty/index.html'),
-        management: resolve(__dirname, 'SGA-management/SGA-management/index.html'),
+        //procesos: resolve(__dirname, 'Process/SGA-Process/index.html'),
+        //tesoreria: resolve(__dirname, 'Treasury/SGA - Treasuty/index.html'),
+        //management: resolve(__dirname, 'SGA-management/SGA-management/index.html'),
         // Añade cada carpeta que tenga su propio index.html
       }
     }
