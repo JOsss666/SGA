5240479567a93bf09e648f9c66231580998442bf
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-02-21 13:10:09 -0500

    new attempt of relative routes

--- Facturation/Facturation/vite.config.js
+++ Facturation/Facturation/vite.config.js
@@ -4,27 +4,23 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
-  // Asegúrate de que en Render esta variable NO esté como 'true'
   const isElectron = process.env.ELECTRON === 'true';
 
   return {
     plugins: [react()],
-    // Si estás en producción para Web (Render), '/' es lo más seguro.
     base: isElectron ? './' : '/', 
     build: {
       rollupOptions: {
         input: {
-            main: resolve(__dirname, 'index.html'),
-            facturacion: resolve(__dirname, 'Facturation/Facturation/index.html'),
-            inventario: resolve(__dirname, 'Inventory/SGA-inventory/index.html'),
-            contabilidad: resolve(__dirname, 'Inventory/index.html'),
-            procesos: resolve(__dirname, 'Process/SGA-Process/index.html'),
-            tesoreria: resolve(__dirname, 'Treasury/SGA - Treasuty/index.html'),
-            management: resolve(__dirname, 'SGA-management/SGA-management/index.html'),
+          // El index principal de la raíz
+          main: resolve(__dirname, 'index.html'),
+          
+          // La ruta exacta basada en tu captura de pantalla:
+          // SGA (raíz) -> Facturation -> Facturation -> index.html
+          facturacion: resolve(__dirname, 'Facturation/Facturation/index.html'),
         }
       },
       outDir: 'dist',
-      // Es recomendable vaciar la carpeta de salida en cada build
       emptyOutDir: true,
     },
     server: {
@@ -32,4 +28,3 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
-
