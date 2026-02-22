7e27c05c6984ab8547c1f82f123434f261ed52c6
Jose Mendieta <josemendietam2707@gmail.com>
2026-02-21 20:10:54 -0500

    historial de caja

--- Treasury/SGA - Treasuty/src/modules/userApp/containers/UserApp.jsx
+++ Treasury/SGA - Treasuty/src/modules/userApp/containers/UserApp.jsx
@@ -49,6 +49,7 @@ import {NoAccess} from './NoAccess'
 import {SuspendedAccount} from './SuspendedAcount'
 import { Movements } from './Movements'; 
 import { MovementsRecord } from './MovementsRecord'; 
+import { CashHistory } from './CashHistory';
 
 
 export function UserApp(){
@@ -209,10 +210,10 @@ export function UserApp(){
                             <Route path='/banks' element={<Banks/>}/>
                                 <Route path='/banks/:bank_id' element={<BanksDetails/>}/>
                             <Route path='/transfers' element={<span>Transferencias</span>}/>
-                            <Route path='/cashBoxes/historial' element={<span>Historial cierres de caja</span>}/>
+                            <Route path='/cashBoxes/historial' element={<span><CashHistory/></span>}/>
                             <Route path='/payments' element={<span><Payments/></span>}/>
                             <Route path='/collections' element={<span><Collections/></span>}/>
-                            <Route path='/minorExpenses' element={<span>Gastos menores</span>}/>
+                            <Route path='/minorExpenses' element={<span></span>}/>
 
                             <Route path='/messages/*' element={<Messages/>} />
                             <Route path='/calendar' element={<Calendar/>} />
