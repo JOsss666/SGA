c75e11a72900d71c0a14cc6143a201ba8c742ad4
José Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-03-04 08:56:52 -0500

    Now functional pistol Qr + barCode lecture

--- Facturation/Facturation/src/modules/userApp/containers/QuickActions.jsx
+++ Facturation/Facturation/src/modules/userApp/containers/QuickActions.jsx
@@ -1,14 +1,70 @@
+import { useRef, useState } from 'react'
 import { BoldTitle } from '../components/BoldTitle'
+import {useAlert} from '../../../context/context'
 import { DescriptionSpan } from '../components/DescriptionSpan'
 import './QuickActions.css'
+import { ProcessStatusAlert } from './Alerts/ProcessStatusAlert'
 
 export function QuickActions(){
+
+    // Requirements
+    const inRef = useRef();
+    const {popInAlert} = useAlert()
+
+    // Control
+    const [disabled,setDisabled] = useState(false);
+
+
+    // Functions
+
+    const normalizeUrl = (raw) => {
+        return raw
+            .replace('https&]]', 'https://')
+            .replaceAll(']', '/')
+            .replaceAll('-', '.');
+    };
+
+    const handleCodeAction = async(code)=>{
+       // Caso Lectura de QR
+        if(code.startsWith('https&]]')){
+            const fixedUrl = normalizeUrl(code);
+            window.open(fixedUrl, '_blank', 'noopener,noreferrer');
+            return;
+        }
+        // Caso lectura cod Barras
+        let action = code.split('n')[0];
+        let value = code.split('n')[1];
+        if(action == undefined || value == undefined){
+            alert('Accion no valida')
+            return;
+        }
+
+        switch (action){
+            case '1026': popInAlert(<ProcessStatusAlert instance_id={value}/>); break;
+        }
+    }
+
+    const handleCodeInteraction = ()=>{
+        setDisabled(true)
+        console.log(inRef.current.value);
+        // funcion para procesar codigos
+            handleCodeAction(inRef.current.value);
+        setDisabled(false)
+        inRef.current.value = "";
+        inRef.current.focus;
+    }
+    
     return(
         <div className="QuickActions">
             <div className="searchContent">
                 <BoldTitle text={'Acciones Rapidas'}/>
                 <DescriptionSpan text={'Escanee el codigo QR o el codigo de barras'}/>
-                <input type="text" placeholder="Escanear o digitar codigo" autoFocus/>
+                <form onSubmit={(e)=>{
+                    e.preventDefault();
+                    handleCodeInteraction();
+                }}>
+                    <input ref={inRef} type="text" placeholder="Escanear o digitar codigo" autoFocus disabled={disabled}/>
+                </form>
             </div>
             <div className="img1C">
                 <img src="https://res.cloudinary.com/djjxugmni/image/upload/v1772629039/unnamed-2_rg2vg1.png" alt="" />
