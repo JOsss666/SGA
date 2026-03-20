81eb2a18f440b24685d8a934dfa2a81e1f1cc314
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-03-20 15:20:51 -0500

    Testing date for printable client orders and cash recipts

--- Facturation/Facturation/src/utils/functions.js
+++ Facturation/Facturation/src/utils/functions.js
@@ -767,6 +767,7 @@ export async function printClientOrder(info,appInfo,barCode){
     if (!window.require) {
         alert("Esta función solo está disponible en la App de Escritorio.");
     }
+    const expeditionDate = new Date().toLocaleString();
     const qrUrl = await QRCode.toDataURL(`https://facturation.sga360.co/preview/Document/${appInfo.company_key}/${info.doc_id}`)
     let procesqrUrl;
     if(info.instance_id != undefined){
@@ -820,6 +821,10 @@ export async function printClientOrder(info,appInfo,barCode){
 
             <span style="font-size:12px;">
                 Tercero: ${info.thirdParty_name}
+            </span>
+
+            <span style="font-size:12px;">
+                Fecha de generación: ${expeditionDate}
             </span>
 
             <span style="
