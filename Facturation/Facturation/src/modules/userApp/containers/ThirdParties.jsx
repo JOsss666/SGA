8e3a8e6a90ab8170e51bb97ea54751327a575d03
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-04-21 17:32:37 -0500

    Fixed thirdparties bugs'

--- Facturation/Facturation/src/modules/userApp/containers/ThirdParties.jsx
+++ Facturation/Facturation/src/modules/userApp/containers/ThirdParties.jsx
@@ -14,6 +14,7 @@ import { NoResults } from './NoResults';
 
 
 export function ThirdParties() {
+    const {appInfo} = useAppInfo();
     const navigate = useNavigate();
     const params = useParams();
     const {appInfo,userConfig} = useAppInfo();
@@ -31,7 +32,7 @@ export function ThirdParties() {
         try {
             setLoading(true);
             setError(null);
-            const response = await postInfo('/getThirdParties', { company_id: 1 });
+            const response = await postInfo('/getThirdParties', { company_id: appInfo.company_id});
             
             if (response[0]){
                 setThirdPartiesData(response[1])
