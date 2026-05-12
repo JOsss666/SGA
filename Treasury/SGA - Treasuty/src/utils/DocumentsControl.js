baf1d2bb469b7a15989e2e831ce86a94edcd4ba5
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-05-12 09:03:44 -0500

    Styles and section of settings updated from facturation

---
+++ Treasury/SGA - Treasuty/src/utils/DocumentsControl.js
@@ -0,0 +1,71 @@
+
+const validateRequired = (obj, fields) => {
+    const missingKeys = fields.filter(
+        (field) => obj[field] === undefined || obj[field] === null || obj[field] === ""
+    );
+
+    // Traducir a nombres funcionales
+    const missingFields = missingKeys.map(
+        (key) => dictionaryFieds[key] || key
+    );
+
+    console.log(obj)
+    console.log(missingFields)
+
+    return {
+        isValid: missingKeys.length === 0,
+        missingFields, // ya traducidos
+        missingKeys,   // opcional: útil para debug o lógica interna
+        message: missingFields.length
+        ? `Campos faltantes: ${missingFields.join(', ')}`
+        : null
+    };
+};
+
+const requiredHeaderFields = [
+  'store_id',
+  'costCenter_id',
+  'concept_id',
+  'company_id',
+  'created_by',
+  'thirdParty_id',
+  'thirdParty_name',
+  //'bussines_id',
+  'doc_type',
+  'subTotal',
+  'total'
+];
+
+const dictionaryFieds = {
+    'store_id':'Tienda',
+    'costCenter_id':'Centro de costo',
+    'concept_id':'Concepto',
+    'company_id':'Compañia',
+    'created_by':'Creado por',
+    'thirdParty_id':'Id del tercero',
+    'thirdParty_name':'Nombre del tercero',
+    'bussines_id':'Negocio',
+    'doc_type':'Tipo de documento',
+    'subTotal':'Sub total',
+    'total':'Total'
+}
+
+const actionsHandlers = {
+  VALIDATE_REQUIRED_HEADER: (context) => {
+    return validateRequired(context, requiredHeaderFields);
+  }
+};
+
+export function executeDocumentAction(action, context) {
+  const handler = actionsHandlers[action];
+
+  if (!handler) {
+    throw new Error(`Acción no permitida: ${action}`);
+  }
+
+  return handler(context);
+}
+
+function validateHeadInvoice(info) {
+  return validateRequired(info, requiredHeaderFields);
+}
