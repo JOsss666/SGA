f4a8d1190b6bef5c3434ab373f5f5c136e253bad
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-06-08 15:05:06 -0500

    Search in list now have aviable default value and taxInfo from thirdParties details now have default values

--- Facturation/Facturation/src/modules/userApp/containers/ThirdPartiesDetailsSections/TaxInfo.jsx
+++ Facturation/Facturation/src/modules/userApp/containers/ThirdPartiesDetailsSections/TaxInfo.jsx
@@ -8,9 +8,10 @@ import { useEffect, useState } from "react";
 import { FormInput } from "../../components/FormInput";
 import { SearchinList } from "../../components/SearchInList";
 import { FormButton } from "../../components/FormButton";
-import { postInfo } from "../../../../utils/functions";
+import { getTextFromValue, postInfo } from "../../../../utils/functions";
 import { useParams } from "react-router-dom";
 import { LoadingSpace } from "../LoadingSpace";
+import { ThirdPartyFactusIdentificationTypeCodes, ThirdPartyIvaResponsabilityCodes, ThirdPartyNatureCodes } from "../../../../utils/Constants";
 
 export function TaxInfo({info}){
 
@@ -90,24 +91,9 @@ export function TaxInfo({info}){
                             e.preventDefault();
                             updateTaxInfo();
                         }}>
-                            <SearchinList placeHolder={'Seleccione una opción'} action={setTypePerson} title={'Naturaleza'} disabled={disabled} list={[
-                                {text:'Persona juridica',value:1},
-                                {text:'Persona Natural',value:2}
-                            ]}/>
-                            <SearchinList placeHolder={`Seleccione una opción`} action={setIVA_responsability} title={'Responsabilidad de IVA'} disabled={disabled} list={[
-                                {text:'No aplica / No responsable de IVA',value:21},
-                                {text:'IVA',value:18},
-                                {text:'No responsable de consumo',value:22},
-                            ]}/>
-                            <SearchinList action={handleSetDocumentType} title={'Tipo de Identificación'} placeHolder={'Tipo de identificación'} list={[
-                                {text:'NIT',value:'NIT'},
-                                {text:'Cédula de Ciudadanía',value:'CC'},
-                                {text:'Cédula de Extranjería',value:'CE'},
-                                {text:'Tarjeta de extranjería',value:'TE'},
-                                {text:'Pasaporte',value:'PAS'},
-                                {text:'Registro civil',value:'RC'},
-                                {text:'Tarjeta de identidad',value:'TI'}
-                            ]}/>
+                            <SearchinList defaultValue={{text:getTextFromValue(info.thirdParty_nature,ThirdPartyNatureCodes), value:info.thirdParty_nature}} placeHolder={'Seleccione una opción'} action={setTypePerson} title={'Naturaleza'} disabled={disabled} list={ThirdPartyNatureCodes}/>
+                            <SearchinList defaultValue={{text:getTextFromValue(info.IVA_responsability,ThirdPartyIvaResponsabilityCodes), value:info.IVA_responsability}} placeHolder={`Seleccione una opción`} action={setIVA_responsability} title={'Responsabilidad de IVA'} disabled={disabled} list={ThirdPartyIvaResponsabilityCodes}/>
+                            <SearchinList defaultValue={{text:getTextFromValue(info.identidicationType_id,ThirdPartyFactusIdentificationTypeCodes), value:info.identidicationType_id}} action={handleSetDocumentType} title={'Tipo Identificación Facturación'} placeHolder={'Tipo de identificación'} list={ThirdPartyFactusIdentificationTypeCodes}/>
                             <FormInput title={'Regimen del tercero'} action={setRegime} disabled={disabled} value={regime ?? '-'}/>
                             <FormInput title={'Tipo de Retención'} action={setRetention_type} disabled={disabled} value={retention_type}/>
                             <FormInput title={'Actividad Economica'} action={setEconomic_activity} disabled={disabled} value={economic_activity}/>
---
+++ Facturation/Facturation/src/utils/Constants.js
@@ -0,0 +1,23 @@
+
+
+
+export const ThirdPartyNatureCodes = [
+    {text:'Persona juridica',value:1},
+    {text:'Persona Natural',value:2}
+];
+
+export const ThirdPartyIvaResponsabilityCodes = [
+    {text:'No aplica / No responsable de IVA',value:21},
+    {text:'Responsable de IVA',value:18},
+    {text:'No responsable de consumo',value:22},
+];
+
+export const ThirdPartyFactusIdentificationTypeCodes = [
+    { text: 'NIT', value: 6 },
+    { text: 'Cédula de Ciudadanía', value: 3 },
+    { text: 'Cédula de Extranjería', value: 5 },
+    { text: 'Tarjeta de extranjería', value: 4 },
+    { text: 'Pasaporte', value: 7 },
+    { text: 'Registro civil', value: 11 },
+    { text: 'Tarjeta de identidad', value: 12 }
+];
