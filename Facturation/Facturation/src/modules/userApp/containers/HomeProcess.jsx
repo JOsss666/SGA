9808ad72a4a96508604e5b4563da3e3d440155ef
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-08-15 20:40:30 -0500

    Fixed home search to new standard 

--- Facturation/Facturation/src/modules/userApp/containers/HomeProcess.jsx
+++ Facturation/Facturation/src/modules/userApp/containers/HomeProcess.jsx
@@ -6,9 +6,12 @@ import './HomeProcess.css'
 import { HomeSearch } from "./HomeSearch";
 import { AppIcon } from "../components/AppIcon";
 import { AiResume } from "../components/AiResume";
+import { useAlert } from "../../../context/context";
+import { SearchResultsPannel } from "./Alerts/SearchResultsPannel";
 
 export function HomeProcess(){
 
+    const {popInAlert} = useAlert();
     const [visibleSearch,setVisibleSearch] = useState(false);
 
     return(
@@ -21,7 +24,10 @@ export function HomeProcess(){
                     <AppIcon title={'Documentos'} imgUrl={'https://cdnmain.sga360.co/static/DocumentosLogo_fuofdc.webp'}/>
                     <AppIcon title={'Archivos'} imgUrl={'https://cdnmain.sga360.co/static/CarpetaLogo1_zzjnut.webp'}/>
                     <AppIcon title={'Informes'} imgUrl={'https://cdnmain.sga360.co/static/InformesLogo1_iisxav.webp'}/>
-                    <AppIcon onClick={()=>{setVisibleSearch(!visibleSearch)}} title={'Buscar'} imgUrl={'https://cdnmain.sga360.co/static/BuscarLogo1_jf8ij8.webp'}/>
+                    <AppIcon onClick={()=>{
+                        //setVisibleSearch(!visibleSearch)
+                        popInAlert(<SearchResultsPannel searchValue={""}/>)
+                    }} title={'Buscar'} imgUrl={'https://cdnmain.sga360.co/static/BuscarLogo1_jf8ij8.webp'}/>
                     <AppIcon title={'Cuenta'} imgUrl={'https://cdnmain.sga360.co/static/CuentaLogo1_aqqot5.webp'}/>
                 </div>
             </div>
