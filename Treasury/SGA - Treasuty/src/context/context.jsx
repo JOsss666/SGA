93fbaeca206220106c6b35cf5964b3fabadbaaaa
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-09-07 10:13:33 -0500

    Fixed icon in mainPanle treasury

--- Treasury/SGA - Treasuty/src/context/context.jsx
+++ Treasury/SGA - Treasuty/src/context/context.jsx
@@ -438,7 +438,7 @@ export function AppInfoProvider({children}){
         {text:'Crear',path:'new',icon:<i className="ti ti-sparkle-2"/>,action:handleNavigate},
         {text:'Terceros',path:'thirdparties',icon:<i className="fa-regular fa-user"/>,img:<img src='https://res.cloudinary.com/djjxugmni/image/upload/v1761579581/ChatGPT_Image_27_oct_2025_10_28_59_3_juwusq.png'/>,action:handleNavigate},
         // Default tools
-        {text:'Panel Principal',path:'mainPanel',icon:<img src='https://res.cloudinary.com/djjxugmni/image/upload/v1761515340/Grupo5logos_3_qp85tn.png'/>,action:handleNavigate},
+        {text:'Panel Principal',path:'mainPanel',icon:<i className="bi bi-grid-1x2"/>,action:handleNavigate},
         {text:'Bancos',path:'banks',icon:<i className="bi bi-bank"/>,action:handleNavigate},
         {text:'Cartera',path:'briefcases',icon:<i className="bi bi-wallet2"/>,img:<img src='https://res.cloudinary.com/djjxugmni/image/upload/v1761579581/ChatGPT_Image_27_oct_2025_10_28_59_3_juwusq.png'/>,action:handleNavigate},
         {text:'Cajas POS',path:'CashBoxes',icon:<i className="ti ti-cash-register"/>,img:<img src='https://res.cloudinary.com/djjxugmni/image/upload/v1761579581/ChatGPT_Image_27_oct_2025_10_28_59_3_juwusq.png'/>,action:handleNavigate},
