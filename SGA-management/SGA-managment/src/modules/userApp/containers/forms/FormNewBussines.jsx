988f744d99889de68c890975ed4bf813d377d771
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-09-09 15:58:16 -0500

    fix(management): use Cloudflare default business image

--- SGA-management/SGA-managment/src/modules/userApp/containers/forms/FormNewBussines.jsx
+++ SGA-management/SGA-managment/src/modules/userApp/containers/forms/FormNewBussines.jsx
@@ -21,7 +21,7 @@ export function FormNewBussines({info,edit,reloadFun}){
     // FormInfo
     const [name,setName] = useState('');
     const [description,setDescription] = useState('');
-    const [photo,setPhoto] = useState('https://res.cloudinary.com/djjxugmni/image/upload/v1765903321/ChatGPT_Image_16_dic_2025_11_41_43_zhakuf.png');
+    const [photo,setPhoto] = useState('https://cdnmain.sga360.co/static/ChatGPT_Image_16_dic_2025_11_41_43_zhakuf.webp');
 
     const formInfo = {
         company_id:appInfo.company_id,
