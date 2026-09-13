e52e12167afb3357a846091bb058797422a01f22
Jose Miguel Murillo Romero <murillojose.nvc@gmail.com>
2026-09-13 17:11:31 -0500

    New Tag indicator version

--- Facturation/Facturation/src/modules/userApp/components/TagIndicator.jsx
+++ Facturation/Facturation/src/modules/userApp/components/TagIndicator.jsx
@@ -1,8 +1,29 @@
 import './TagIndicator.css'
 
-export function TagIndicator({desc,title,type,children,classN}){
+const typeIcons = {
+    active: 'fa-solid fa-circle-check',
+    disabled: 'fa-solid fa-ban',
+    suspended: 'fa-solid fa-clock',
+    indicator: 'fa-solid fa-certificate',
+    category: 'fa-solid fa-layer-group',
+    green: 'fa-solid fa-scroll',
+    blue: 'fa-solid fa-certificate',
+    info: 'fa-solid fa-circle-info',
+    purple: 'fa-solid fa-leaf',
+};
+
+export function TagIndicator({desc,title,type,children,classN = '',icon}){
+    const iconClass = icon === undefined
+        ? typeIcons[type] ?? 'fa-solid fa-circle-check'
+        : icon;
+
     return(
-        <div title={desc} className={`TagIndicator ${type}_color ${classN}`}>
+        <div title={desc} className={`TagIndicator ${type ? `${type}_color` : ''} ${classN}`}>
+            {iconClass && (
+                <span className="tagIndicatorIcon" aria-hidden="true">
+                    <i className={iconClass}/>
+                </span>
+            )}
             <h5>{title}</h5>
             {children}
         </div>
